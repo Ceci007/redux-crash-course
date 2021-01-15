cf0ed070e9cbc548e8cf5b4e19f840372ff4d98d
solbencas <cecibenitezca@gmail.com>
2021-01-15 13:14:04 -0300

    Add actionTypes.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import store from "./store";
+import * as actions from "./actionTypes";
 
 store.dispatch({
-  type: "bugAdded",
+  type: actions.BUG_ADDED,
   payload: {
     description: "Bug1"
   }
