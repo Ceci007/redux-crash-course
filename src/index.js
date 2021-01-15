c008580ce051d215fd30d4f15b4e6b61cddcc00c
solbencas <cecibenitezca@gmail.com>
2021-01-15 13:50:01 -0300

    Created bugResolved action

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import store from "./store";
-import * as actions from "./actionTypes";
+import { bugAdded, bugResolved } from "./actions";
 
-store.dispatch({
-  type: actions.BUG_ADDED,
-  payload: {
-    description: "Bug1"
-  }
-});
+store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugResolved(1));
 
 console.log(store.getState());
