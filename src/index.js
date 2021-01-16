dcdec02b723ca315815963436de3587868b96447
solbencas <cecibenitezca@gmail.com>
2021-01-16 13:22:36 -0300

    Writting clean redux code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
-import store from './customStore';
-import * as actions from './actions';
+import configureStore from './store/configureStore';
+import * as actions from './store/bugs';
+import { projectAdded } from './store/projects';
+
+const store = configureStore();
 
 store.subscribe(() => {
   console.log("Store changed!");
 });
 
-store.dispatch(actions.bugAdded("Bug 1"));
+store.dispatch(projectAdded({ name: "Project 1" }))
+store.dispatch(actions.bugAdded({ description: "Bug 1" }));
+store.dispatch(actions.bugAdded({ description: "Bug 2" }));
+store.dispatch(actions.bugAdded({ description: "Bug 3" }));
+store.dispatch(actions.bugResolved({ id: 1 }));
 
 console.log(store.getState());
---
+++ src/store/configureStore.js
@@ -0,0 +1,6 @@
+import { configureStore } from '@reduxjs/toolkit';
+import reducer from "./projects";
+
+export default function() {
+  return configureStore({ reducer });
+}
