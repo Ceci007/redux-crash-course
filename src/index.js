519fbdb1b2ea09d3b0a46a250adeb9eddb4e3d17
solbencas <cecibenitezca@gmail.com>
2021-01-20 18:14:08 -0300

    Add some middlewares

--- src/index.js
+++ src/index.js
@@ -1,30 +1,8 @@
-import configureStore from './store/configureStore';
-import { 
-  bugAdded, 
-  bugResolved, 
-  getUnresolvedBugs, 
-  bugAssignedToUser,
-  getBugsByUser
-} from './store/bugs';
-import { projectAdded } from './store/projects';
-import { userAdded } from './store/users';
+import configureStore from "./store/configureStore";
 
 const store = configureStore();
 
-store.subscribe(() => {
-  console.log("Store changed!");
-});
-
-store.dispatch(userAdded({ name: "User 1" }));
-store.dispatch(userAdded({ name: "User 2" }));
-store.dispatch(projectAdded({ name: "Project 1" }));
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }))
-store.dispatch(bugResolved({ id: 1 }));
-
-const unresolvedBugs = getUnresolvedBugs(store.getState());
-
-const bugs = getBugsByUser(1)(store.getState());
-console.log(bugs);
+store.dispatch({
+  type: "error",
+  payload: { message: "An error occurred" }
+})
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,6 +1,15 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import reducer from "./reducer";
+import logger from "./middleware/logger";
+import toast from "./middleware/toast";
 
 export default function() {
-  return configureStore({ reducer });
+  return configureStore({ 
+    reducer,
+    middleware: [
+      ...getDefaultMiddleware(),
+      logger({ destination: 'console' }),
+      toast
+    ] 
+  });
 }
