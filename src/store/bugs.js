4b80c4a4789f0117fb8293dd5cf59329c26a70f0
solbencas <cecibenitezca@gmail.com>
2021-01-23 12:50:03 -0300

    Add moment library and basic caching

--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,6 +1,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { createSelector } from 'reselect';
 import { apiCallBegan } from './api';
+import moment from 'moment';
 
 let lastId = 0;
 
@@ -19,6 +20,7 @@ const slice = createSlice({
     bugsReceived: (bugs, action) => {
       bugs.list = action.payload;
       bugs.loading = false;
+      bugs.lastFetch = Date.now();
     },
 
     bugsRequestFailed: (bugs, action) => {
@@ -58,12 +60,22 @@ export default slice.reducer;
 
 const url = "/bugs";
 
-export const loadBugs = () => apiCallBegan({
-  url,
-  onStart: bugsRequested.type,
-  onSuccess: bugsReceived.type,
-  onError: bugsRequestFailed.type
-});
+export const loadBugs = () => (dispatch, getState) => {
+  const { lastFetch } = getState().entities.bugs;
+  
+  const diffInMinutes = moment().diff(moment(lastFetch), 'minutes');
+  if (diffInMinutes < 10) return;
+
+  dispatch(
+    apiCallBegan({
+      url,
+      onStart: bugsRequested.type,
+      onSuccess: bugsReceived.type,
+      onError: bugsRequestFailed.type
+    })
+  );
+}
+
 
 export const getUnresolvedBugs = createSelector(
   state => state.entities.bugs,
